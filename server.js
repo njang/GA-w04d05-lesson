46b9c62a426e95a2a9ee1b08b20eed3a1cf09706
njang <neo.w.jang@gmail.com>
2018-01-05 11:20:24 -0600

    Add style.css, install morgan and body-parser

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express'); 
+const morgan  = require('morgan'); 
 const app = express();
 
 app.use(express.static('public'))
+app.use(morgan('tiny'))
 
 app.listen(3000, () => {
     console.log("I am listening");
